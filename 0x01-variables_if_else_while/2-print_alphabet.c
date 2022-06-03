f26d900953e543fdbeedb8d8f6c545da5108393a
Munashe R Mhlanga <mnashemhlanga8@gmail>
2022-06-03 15:17:32 -0700

    Print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ * main -Lanzador.
+ *
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	char c;
+	char a = 'a';
 
-	for (c = 'a'; c <= 'z', c++)
-		putchar(c);
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
