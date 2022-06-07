cbca5299cd6a2d9828d91920dfb7b1bfee118d87
Munashe R Mhlanga <mnashemhlanga8@gmail>
2022-06-07 04:27:39 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * return: 0 always
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
