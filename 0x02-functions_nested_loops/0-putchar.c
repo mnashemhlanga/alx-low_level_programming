fd1dbf8597438fcc582287fa674ce4a9bb325ff2
Munashe R Mhlanga <mnashemhlanga8@gmail>
2022-06-07 04:12:00 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
- * main - Entry point
+ * main - entry point
  *
- * Return: nothing, return void
+ * return = 0
  */
-
 int main(void)
-
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+char str[] = "_putchar\n";
+int i = 0;
 
+while (str[i] != '\0')
+{
+char c = str[i];
+_putchar(c);
+i++;
+}
 return (0);
 }
 
