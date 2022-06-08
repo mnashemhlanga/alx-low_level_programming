38ed15b448a2c9e011b6a08750579170443b57b1
Munashe R Mhlanga <mnashemhlanga8@gmail>
2022-06-08 08:47:44 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,22 @@
 /**
  * main - entry point
  *
- * return = 0
+ * return always 0 success
  */
+
 int main(void)
 {
-char str[] = "_putchar\n";
-int i = 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-while (str[i] != '\0')
-{
-char c = str[i];
-_putchar(c);
-i++;
-}
+
 return (0);
 }
 
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * check for lower case characters
+ *
+ * Return 0 always
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
+
+	
+
