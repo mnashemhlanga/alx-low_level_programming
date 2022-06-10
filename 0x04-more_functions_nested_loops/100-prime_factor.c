faee7cfbf6d76616f900af32b82ee16246d55fa7
Munashe R Mhlanga <mnashemhlanga8@gmail>
2022-06-09 19:17:24 -0700

    prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -  program that finds and prints the largest prime factor of a number
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long number  = 612852475143;
+	long prime = 2;
+
+	while (prime <= number)
+	{
+		if (number % prime != 0)
+			prime++;
+		else
+			number = number / prime;
+	}
+	printf("%lu\n", prime);
+	return (0);
+}
