75b681b03071905108978de75c79bf6162a973e7
Munashe R Mhlanga <mnashemhlanga8@gmail>
2022-06-16 04:14:35 -0700

    102 .magic

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - a function that encodes a string using rot13
+ * @s: string to be encoded
+ * return 0.
+ */
+
+char *rot13(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+	{
+		while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+		{
+			if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
+			{
+				s[a] -= 13;
+				break;
+			}
+			s[a] += 13;
+			break;
+		}
+		a++;
+	}
+	return (s);
+}
+
+	
