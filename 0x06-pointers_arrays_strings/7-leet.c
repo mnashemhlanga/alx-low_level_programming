040029cff2643f06195e63b65e07ad252c0f197e
Munashe R Mhlanga <mnashemhlanga8@gmail>
2022-06-16 01:43:33 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - encode a string into 1337
+ * @c: string of variable pointer
+ * Return: string that is encoded
+ */
+
+ar *leet(char *c)
+{
+	char *cp = c;
+	char key[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*c)
+	{
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+			if (*c == key[i] || *c == key[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++;
+	}
+	return (cp);
+}
+
