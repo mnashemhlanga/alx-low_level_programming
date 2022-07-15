2b2bcb9921f65b6397daf157e5c54bcd0ed9e5ac
mnashemhlanga <mnashemhlanga8@gmail.com>
2022-07-14 20:03:38 -0700

    hello holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,41 @@
+	.file	"101-hello_bootcamp.c"
+	.section	.rodata
+CLI
+.LC0:
+	.string	"Hello, Bootcamp"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rcx
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rax, %rcx
+	.cfi_def_cfa_register 6
+	movq	%rax, %rcx
+
+	.cfi_def_cfa_register 6
+
+	movl	$.LC0, %edi
+
+	call	puts
+
+	movl	$0, %eax
+
+	popq	%rcx
+
+	.cfi_def_cfa 7, 8
+
+	ret
+
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+
+	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.3) 4.8.4"
+
+	.section	.note.GNU-stack,"",@progbits
+
+
